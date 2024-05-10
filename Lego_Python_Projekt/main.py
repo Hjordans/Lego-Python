f3659f5708bb3ae772d9adc23efbcc0d5bde9d4a
[Florian Heine] <[florian.heine@koerber.com]>
2024-05-10 13:37:30 +0200

    Ich habe hier alle Motoren und Sensoren initialisiert.

--- Lego_Python_Projekt/main.py
+++ Lego_Python_Projekt/main.py
@@ -9,6 +9,18 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # Create your objects here.
 ev3 = EV3Brick()
+motor_left = Motor(Port.B)
+motor_right = Motor(Port.C)
+color = ColorSensor(Port.S3)
+gyro = GyroSensor(Port.S2)
+# The wheel diameter is 54 mm and the axle track is 132 mm.
+robot = DriveBase(motor_left, motor_right, wheel_diameter = 54, axle_track = 132)
 
 # Write your program here.
-ev3.screen.print(*"Hallo", sep="", end="")
+gyro.reset_angle(0)
+while robot.distance() <= 1000:
+    correction = (0 -gyro.angle()) * 2
+    robot.drive(200, correction)
+robot.stop()
+motor_left.brake()
+motor_right.brake()
